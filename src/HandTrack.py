ebd723dfbf62f10f0dfeac4487fb1c9e41199422
Kelvin Huynh <kelvin.huynh.5@gmail.com>
2022-10-30 16:40:14 -0400

    Update format to conform to flake8 linting

--- src/HandTrack.py
+++ src/HandTrack.py
@@ -7,22 +7,22 @@ mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
-    model_complexity = 0,
-    min_detection_confidence = 0.5,
-    min_tracking_confidence = 0.5) as hands:
+        model_complexity=0,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as hands:
 
     while cap.isOpened():
-        success, image = cap.read() # Success = feed established
+        success, image = cap.read()  # Success = feed established
         if not success:
             print("Empty camera frame")
             continue
 
         # Performance Optimization
-        
+
         image.flags.writeable = False
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = hands.process(image)
-        
+
         # Draw Hand joints
 
         image.flags.writeable = True
@@ -35,9 +35,9 @@ with mp_hands.Hands(
                     mp_hands.HAND_CONNECTIONS,
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style())
-        
-        cv2.imshow('Hand Tracking', cv2.flip(image,1))
-        if cv2.waitKey(5) & 0xFF == 27: # Continuous feed until esc is pressed
+
+        cv2.imshow('Hand Tracking', cv2.flip(image, 1))
+        if cv2.waitKey(5) & 0xFF == 27:  # Continuous feed until esc is pressed
             break
 
 cap.release()
