1cff4f74fd00680454b166df21b0fe6a3c2b9f10
Kelvin Huynh <kelvin.huynh.5@gmail.com>
2022-11-10 12:45:15 -0500

    Generalized file pathing

--- src/model/keypoint_classifier/keypoint_classifier.py
+++ src/model/keypoint_classifier/keypoint_classifier.py
@@ -1,9 +1,10 @@
 import numpy as np
 import tensorflow as tf
+import os
 
 
 class KeyPointClassifier(object):
-    def __init__(self, model_path='model/keypoint_classifier/keypoint_classifier.tflite', num_threads=1):
+    def __init__(self, model_path=os.getcwd() + '\\src\\model\\keypoint_classifier\\keypoint_classifier.tflite', num_threads=1):
         self.interpreter = tf.lite.Interpreter(model_path=model_path,
                                                num_threads=num_threads)
         self.interpreter.allocate_tensors()
