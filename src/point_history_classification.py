be16073d4fceb4c0ea94cf694b456e8ab20cc5f7
Kelvin Huynh <kelvin.huynh.5@gmail.com>
2023-04-04 10:09:44 -0400

    Added comments

--- src/point_history_classification.py
+++ src/point_history_classification.py
@@ -2,53 +2,49 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
-RANDOM_SEED = 51
 
+RANDOM_SEED = 51  # Set random seed for reproducible outputs
+
+# Define dataset and model file locations
 dataset = 'src\\model\\point_history_classifier\\point_history.csv'
 model_save_path = 'src\\model\\point_history_classifier\\point_history_classifier.hdf5'
 
-NUM_CLASSES = 26
+NUM_CLASSES = 26  # Number of labels
 TIME_STEPS = 16
 DIMENSION = 2
 
+
+# Format dataset for training
 x_dataset = np.loadtxt(dataset, delimiter=',', dtype='float32', usecols=list(range(1, (TIME_STEPS * DIMENSION) + 1)))
 y_dataset = np.loadtxt(dataset, delimiter=',', dtype='int32', usecols=(0))
 x_train, x_test, y_train, y_test = train_test_split(x_dataset, y_dataset, train_size=0.75, random_state=RANDOM_SEED)
 
-use_lstm = False
+# Build model
 model = None
 
-if use_lstm:
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.InputLayer(input_shape=(TIME_STEPS * DIMENSION, )),
-        tf.keras.layers.Reshape((TIME_STEPS, DIMENSION), input_shape=(TIME_STEPS * DIMENSION, )),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.LSTM(16, input_shape=[TIME_STEPS, DIMENSION]),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(10, activation='relu'),
-        tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')
-    ])
-else:
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.InputLayer(input_shape=(TIME_STEPS * DIMENSION, )),
-        tf.keras.layers.Dropout(0.2),
-        tf.keras.layers.Dense(24, activation='relu'),
-        tf.keras.layers.Dropout(0.5),
-        tf.keras.layers.Dense(10, activation='relu'),
-        tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')
-    ])
+model = tf.keras.models.Sequential([
+    tf.keras.layers.InputLayer(input_shape=(TIME_STEPS * DIMENSION, )),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(10, activation='relu'),
+    tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')
+])
 
 model.summary()
 
+# Define callbacks to compensate for underfitting/overfitting
 cp_callback = tf.keras.callbacks.ModelCheckpoint(model_save_path, verbose=1, save_weights_only=False)
 es_callback = tf.keras.callbacks.EarlyStopping(patience=20, verbose=1)
 
+# Configure model for training
 model.compile(
     optimizer='adam',
     loss='sparse_categorical_crossentropy',
     metrics=['accuracy']
 )
 
+# Train model using dataset
 model.fit(
     x_train,
     y_train,
@@ -60,12 +56,15 @@ model.fit(
 
 model = tf.keras.models.load_model(model_save_path)
 
+# Model testing and validation
 predict_result = model.predict(np.array([x_test[0]]))
 print(np.squeeze(predict_result))
 print(np.argmax(np.squeeze(predict_result)))
 
 model.save(model_save_path, include_optimizer=False)
 model = tf.keras.models.load_model(model_save_path)
+
+# Convert Keras model to TFLite model
 tflite_save_path = 'src\\model\\point_history_classifier\\point_history_classifier.tflite'
 
 converter = tf.lite.TFLiteConverter.from_keras_model(model)  # converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_path)
@@ -80,7 +79,6 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 print(input_details)
-
 
 interpreter.set_tensor(input_details[0]['index'], np.array([x_test[0]]))
 
