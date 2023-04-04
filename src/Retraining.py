ba95e76424da048dfeba8d5d0619b084fd740469
Kelvin Huynh <kelvin.huynh.5@gmail.com>
2023-04-03 22:16:01 -0400

    Update formatting to conform to Flake8

--- src/Retraining.py
+++ src/Retraining.py
@@ -7,18 +7,20 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
 from keras.callbacks import TensorBoard
 from keras import regularizers
+from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
 
-DATA_PATH = os.path.join('MP_Data') 
-actions = np.loadtxt("actions.txt", dtype='str')
+
+DATA_PATH = os.path.join('MP_Data')  # Define file path to dataset
+actions = np.loadtxt("actions.txt", dtype='str')  # Load gesture labels into an array
 
 # Thirty videos worth of data
 no_sequences = 30
 
-# Videos are going to be 30 frames in length
+# Number of frames per video
 sequence_length = 30
 
-
-label_map = {label:num for num, label in enumerate(actions)}
+# Map dataset to gesture labels
+label_map = {label: num for num, label in enumerate(actions)}
 sequences, labels = [], []
 for action in actions:
     for sequence in np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int):
@@ -28,36 +30,35 @@ for action in actions:
             window.append(res)
         sequences.append(window)
         labels.append(label_map[action])
+
+# Format dataset for model training
 X = np.array(sequences)
 y = to_categorical(labels).astype(int)
-#mean = np.mean(X)
-#std = np.std(X)
-#X = (X - mean) / std
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
 
-
-log_dir = os.path.join('Logs') 
-tb_callback = TensorBoard(log_dir=log_dir) 
-model = Sequential() 
-model.add(LSTM(64, kernel_regularizer=regularizers.l2(0.01), return_sequences=True, activation='relu', input_shape=(30,258))) 
-model.add(Dropout(0.2)) 
-model.add(LSTM(128, kernel_regularizer=regularizers.l2(0.01), return_sequences=True, activation='relu')) 
-model.add(Dropout(0.2)) 
-model.add(LSTM(64, kernel_regularizer=regularizers.l2(0.01), return_sequences=False, activation='relu')) 
-model.add(Dense(64, kernel_regularizer=regularizers.l2(0.01), activation='relu')) 
+# Build machine learning model
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+model = Sequential()
+model.add(LSTM(64, kernel_regularizer=regularizers.l2(0.01), return_sequences=True, activation='relu', input_shape=(30, 258)))
+model.add(Dropout(0.2))
+model.add(LSTM(128, kernel_regularizer=regularizers.l2(0.01), return_sequences=True, activation='relu'))
+model.add(Dropout(0.2))
+model.add(LSTM(64, kernel_regularizer=regularizers.l2(0.01), return_sequences=False, activation='relu'))
+model.add(Dense(64, kernel_regularizer=regularizers.l2(0.01), activation='relu'))
 model.add(Dense(32, kernel_regularizer=regularizers.l2(0.01), activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
-#cp_callback = tf.keras.callbacks.ModelCheckpoint('action.h5', verbose=1, save_weights_only=False)
-#es_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=20, mode='min', verbose=1)
+# cp_callback = tf.keras.callbacks.ModelCheckpoint('action.h5', verbose=1, save_weights_only=False)
+# es_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=20, mode='min', verbose=1)
 model.compile(optimizer='Nadam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-#model.fit(X_train, y_train, epochs=200, callbacks=[tb_callback, cp_callback, es_callback])
+# model.fit(X_train, y_train, epochs=200, callbacks=[tb_callback, cp_callback, es_callback])
 model.fit(X_train, y_train, epochs=200, callbacks=[tb_callback])
 
-from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
+# Create confusion matrix for analysis of model accuracy
 yhat = model.predict(X_test)
 ytrue = np.argmax(y_test, axis=1).tolist()
 yhat = np.argmax(yhat, axis=1).tolist()
-#multilabel_confusion_matrix(ytrue, yhat)
+# multilabel_confusion_matrix(ytrue, yhat)
 print(multilabel_confusion_matrix(ytrue, yhat))
 
 model.save('action.h5')
